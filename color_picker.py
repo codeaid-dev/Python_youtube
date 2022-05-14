9769f2eb019089ac8ea7d192bb050be25522b787
codeaid-dev <codeaid.lab@gmail.com>
2022-05-14 23:00:26 +0900

    fix

--- color_picker.py
+++ color_picker.py
@@ -3,13 +3,14 @@ from tkinter import colorchooser
 
 root=tkinter.Tk()
 root.title('Color Picker')
-root.geometry('600x400')
+root.geometry('500x500')
 
 def color():
     color = colorchooser.askcolor()
-    label = tkinter.Label(root, text=f'R:{color[0][0]} G:{color[0][1]} B:{color[0][2]} {color[1]}',
-                        font=('Helvetica',32), bg=color[1], fg='black').pack(pady=10)
+    tkinter.Label(root, text=f'R:{color[0][0]} G:{color[0][1]} B:{color[0][2]} {color[1]}',
+                        font=('Helvetica',20)).pack()
+    tkinter.Label(root, bg=color[1], width=50, height=2).pack(pady=5)
 
-button = tkinter.Button(root, text='色選択', command=color).pack()
+tkinter.Button(root, text='色選択', command=color).pack()
 
 root.mainloop()
