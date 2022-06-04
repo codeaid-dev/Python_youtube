adc5291ad7d721e37501c55549cc08c083ac1c59
codeaid-dev <codeaid.lab@gmail.com>
2022-06-04 21:54:00 +0900

    add fix

---
+++ gui.py
@@ -0,0 +1,16 @@
+import tkinter
+
+from pyparsing import col
+
+root = tkinter.Tk()
+root.geometry('400x200')
+hello = tkinter.Label(root, text='こんにちは', background='red', font=('メイリオ',32))
+evening = tkinter.Label(root, text='こんばんは', background='blue', font=('メイリオ',32))
+#hello.pack(side=tkinter.LEFT)
+#evening.pack(side=tkinter.LEFT)
+#hello.grid(row=0, column=0)
+#evening.grid(row=1, column=1)
+hello.place(x=100, y=50)
+evening.place(x=100, y=150)
+
+root.mainloop()
