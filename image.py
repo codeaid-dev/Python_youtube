cf6888caab8d05629e35af46d18d72bb609064e0
codeaid-dev <codeaid.lab@gmail.com>
2022-07-09 09:21:27 +0900

    fix

--- image.py
+++ image.py
@@ -3,9 +3,9 @@ from PIL import ImageTk, Image
 
 root = tkinter.Tk()
 root.title('画像表示')
-root.iconbitmap('codeaid.ico')
+root.iconbitmap('images/codeaid.ico')
 
-img = ImageTk.PhotoImage(Image.open('test.jpg'))
+img = ImageTk.PhotoImage(Image.open('images/img1.jpg'))
 c = tkinter.Canvas(root, width=600, height=400)
 c_img = c.create_image(300,200,image=img)
 c.pack()
