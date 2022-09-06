3852577ed6b2a603c241e5358276b3ab0c6d2ed5
codeaid-dev <codeaid.lab@gmail.com>
2022-09-06 15:08:14 +0900

    add

---
+++ messagebox.py
@@ -0,0 +1,17 @@
+import tkinter
+
+
+import tkinter
+from tkinter import messagebox
+
+root = tkinter.Tk()
+root.title('メッセージボックス')
+root.geometry('400x200')
+
+def popup():
+    a = messagebox.showinfo('メッセージ', 'Hello World!')
+    print(a)
+
+tkinter.Button(root, text='表示', command=popup).pack()
+
+root.mainloop()
