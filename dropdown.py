e9347955b40aa4912df6d4f68ec7b97094831472
codeaid-dev <codeaid.lab@gmail.com>
2022-09-07 10:36:31 +0900

    add

---
+++ dropdown.py
@@ -0,0 +1,21 @@
+import tkinter
+
+root = tkinter.Tk()
+root.title('プルダウンメニュー')
+root.geometry('300x300')
+
+options = ['Python','Java','C#','Ruby','PHP']
+
+def show():
+    label = tkinter.Label(root, text=selected.get()).pack()
+
+selected = tkinter.StringVar()
+selected.set(options[0])
+
+#dropdown = tkinter.OptionMenu(root, selected, 'Python','Java','C#','Ruby','PHP')
+dropdown = tkinter.OptionMenu(root, selected, *options)
+dropdown.pack()
+
+btn = tkinter.Button(root, text='表示', command=show).pack()
+
+root.mainloop()
