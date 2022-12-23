ce092b785c460870eafe99c72ad39ac225b29989
codeaid-dev <codeaid.lab@gmail.com>
2022-12-23 10:56:40 +0900

    fix

--- uranai.py
+++ uranai.py
@@ -5,12 +5,20 @@ def uranau():
     result = random.randint(1,10)
     if 1 <= result <=2:
         ml2['text'] = '大吉'
+        ml2['font'] = ('ヒラギノ明朝 ProN', 50)
+        ml2['fg'] = 'red'
     elif 3 <= result <= 5:
         ml2['text'] = '吉'
+        ml2['font'] = ('メイリオ', 32)
+        ml2['fg'] = 'white'
     elif 6 <= result <= 8:
         ml2['text'] = '凶'
+        ml2['font'] = ('メイリオ', 32)
+        ml2['fg'] = 'white'
     else:
         ml2['text'] = '大凶'
+        ml2['font'] = ('メイリオ', 32)
+        ml2['fg'] = 'white'
     ml2.update()
 
 root = tkinter.Tk()
